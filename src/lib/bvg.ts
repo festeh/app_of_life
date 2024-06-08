db872bd0bf16d1e7baafe4a9785a8113d0c3c78e
Dima Lipin <dimich3d@ya.ru>
2024-06-08 17:46:52 +0200

    fix direction

--- src/lib/bvg.ts
+++ src/lib/bvg.ts
@@ -4,6 +4,6 @@ export const KirchofStrId = "900028151"
 export function isArrivalToKirchhofStrSouthDirection(trip) {
   let originLat = trip?.origin?.location?.latitude || 0;
   let stopLat = trip?.stop?.location?.latitude || 0;
-  return originLat < stopLat;
+  return originLat > stopLat;
 }
 
