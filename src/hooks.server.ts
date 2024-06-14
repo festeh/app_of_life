38f2800709a8cf7b237503a6b5c3a9f9b675033e
Dima Lipin <dimich3d@ya.ru>
2024-06-14 14:58:19 +0200

    signout feature

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,13 +2,10 @@ import { auth } from "./auth";
 import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  console.log("Got event cookies", event.cookies.getAll(), "need", auth.sessionCookieName)
   const sessionId = event.cookies.get(auth.sessionCookieName);
-  console.log(sessionId, event.cookies.getAll());
   const { session, user } = sessionId
     ? await auth.validateSession(sessionId)
     : { session: null, user: null };
-  console.log("session", session, "user", user);
   if (session && session.fresh) {
     const sessionCookie = auth.createSessionCookie(session.id);
     event.cookies.set(sessionCookie.name, sessionCookie.value, {
@@ -29,7 +26,6 @@ export const handle: Handle = async ({ event, resolve }) => {
   if (event.route.id?.startsWith('/(app)')) {
     if (!user) redirect(301, '/signin');
   }
-  console.log("Set cookies in hook", event.cookies.getAll())
   const response = await resolve(event);
   return response;
 };
--- src/routes/(other)/signout/+page.server.ts
+++ src/routes/(other)/signout/+page.server.ts
@@ -1,9 +1,19 @@
-// import { signOut } from "../../../auth"
-// import type { Actions } from "./$types"
-// export const actions: Actions = {
-//   default:
-//     async (request) => {
-//       console.log("Sign out req", request)
-//       await signOut(request)
-//     }
-// }
+import { redirect } from '@sveltejs/kit';
+import { auth } from '../../../auth';
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+  default: async ({ locals, cookies }) => {
+    const session = locals.session;
+    if (!session) {
+      throw redirect(302, '/signin');
+    }
+    await auth.invalidateSession(locals.session.id); // invalidate session
+    const sessionCookie = auth.createBlankSessionCookie();
+    cookies.set(sessionCookie.name, sessionCookie.value, {
+      path: "/",
+      ...sessionCookie.attributes
+    });
+    throw redirect(302, '/signin');
+  }
+};
