ad008e3c2b1b963304f8721b03357a8db89d17ff
Dima Lipin <dimich3d@ya.ru>
2024-07-23 13:53:06 +0200

    show streak

--- src/components/habits/habits.svelte
+++ src/components/habits/habits.svelte
@@ -8,24 +8,36 @@
 		long_description: string;
 	}
 
+	interface HabitLog {
+		habit: string;
+		date: string;
+		repeats: number;
+		streak: number;
+	}
+
 	interface Date {
 		key: string;
 		repr: string;
 	}
 
 	export let habits: Habit[];
-	export let logs: any[];
+	export let logs: HabitLog[];
 	let completed: Record<string, Record<string, boolean>> = {};
 	let popupOpen: Record<string, boolean> = {};
+	let streaks: Record<string, number> = {};
 
 	onMount(async () => {
 		habits.forEach((habit) => {
 			popupOpen[habit.id] = false;
 			completed[habit.id] = {};
 		});
+
 		logs.forEach((log) => {
 			const isoDate = log.date.substring(0, 10);
 			completed[log.habit][isoDate] = true;
+			if (log.date.substring(0, 10) === (new Date()).toISOString().substring(0, 10)) {
+				streaks[log.habit] = log.streak;
+			}
 		});
 	});
 
@@ -43,9 +55,17 @@
 	}
 
 	async function completeHabit(habit: Habit, date: Date) {
-    if (completed[habit.id] && completed[habit.id][date.key]) {
-      return;
-    }
+		if (completed[habit.id] && completed[habit.id][date.key]) {
+			return;
+		}
+    let streak = 1;
+    const prevDate = new Date(date.key);
+    prevDate.setDate(prevDate.getDate() - 1);
+    logs.forEach((log) => {
+      if (log.habit === habit.id && log.date.substring(0, 10) === prevDate.toISOString().substring(0, 10)) {
+        streak = log.streak + 1;
+      }
+    });
 		const res = await fetch('/api/habits', {
 			method: 'POST',
 			headers: {
@@ -53,9 +73,9 @@
 			},
 			body: JSON.stringify({
 				habit: habit.id,
-        date: date.key,
+				date: date.key,
 				repeats: 1,
-				streak: 1
+				streak
 			})
 		});
 		// invalidateAll();
@@ -67,11 +87,12 @@
 	<div class="text-center">
 		<h1 class="font-bold mb-1">Habit Tracker</h1>
 	</div>
-	<div class="grid grid-cols-8 border rounded-xl shadow-md p-1">
+	<div class="grid grid-cols-9 border rounded-xl shadow-md p-1">
 		<div class="font-bold mb-8"></div>
 		{#each dates as date}
 			<div class="text-center">{date.repr}</div>
 		{/each}
+		<div class="text-center">Streak</div>
 		{#each habits as habit, i (habit.id)}
 			<Popover bind:open={popupOpen[habit.id]}>
 				<div class="p-2 bg-surface-100 border shadow text-center">
@@ -95,7 +116,9 @@
 					></span>
 				</button>
 			{/each}
+			<div class="flex justify-center items-center font-bold text-lg">
+				{streaks[habit.id] || 0}
+			</div>
 		{/each}
-		<div class="text-center"></div>
 	</div>
 </div>
